b04c81287eb408973719b51ed6851e19bd005289
Sumitsh28 <sumit.off28@gmail.com>
2024-09-28 17:17:00 +0530

    get req async done

---
+++ projects/get_req_async/src/main.rs
@@ -0,0 +1,27 @@
+use error_chain::error_chain;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("Hello, world!");
+
+    
+    let res = reqwest::get("http://httpbin.org/get").await?;
+    
+
+   
+    println!("Status:  {}", res.status());
+    println!("Headers: {:#?}", res.headers());
+
+    let body = res.text().await?;
+    
+    println!("Body: {}", body);
+
+    Ok(())
+}
